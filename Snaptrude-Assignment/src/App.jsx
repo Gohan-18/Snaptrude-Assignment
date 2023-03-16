41c0ff88837bd60677d0baa8f759895f2144159e
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-03-16 20:39:07 +0530

    Basic map done

--- Snaptrude-Assignment/src/App.jsx
+++ Snaptrude-Assignment/src/App.jsx
@@ -1,12 +1,46 @@
-import './App.css'
+import "./App.css";
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+import Map, { FullscreenControl, GeolocateControl, Marker, NavigationControl } from "react-map-gl";
+import { useState } from "react";
 
 function App() {
+  // mapboxgl.accessToken =
+  //   "pk.eyJ1IjoicHJhYi1oYXQxOCIsImEiOiJjbGZiNjVuaGQyeGxnM29yMDU3MGQzaGVhIn0.8eObBAjrqScgVVrmE-pbOQ";
+  // const map = new mapboxgl.Map({
+  //   container: "map", // container ID
+  //   style: "mapbox://styles/mapbox/streets-v12", // style URL
+  //   center: [-74.5, 40], // starting position [lng, lat]
+  //   zoom: 10, // starting zoom
+  // });
+
+  // const [viewPort, setViewPort] = useState({
+  //   longitude: -100,
+  //   latitude: 40,
+  //   zoom: 10,
+  //   width: "100vw",
+  //   height: "100vh"
+  // })
+
+  const lat = 28.644800
+  const long = 77.216721
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  )
+    <Map
+      mapboxAccessToken="pk.eyJ1IjoicHJhYi1oYXQxOCIsImEiOiJjbGZiNjVuaGQyeGxnM29yMDU3MGQzaGVhIn0.8eObBAjrqScgVVrmE-pbOQ"
+      initialViewState={{
+        longitude: long,
+        latitude: lat,
+        zoom: 10
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      <Marker longitude={long} latitude={lat} />
+      <NavigationControl position="bottom-right" />
+      <GeolocateControl/>
+      <FullscreenControl/>
+    </Map>
+  );
 }
 
-export default App
+export default App;
